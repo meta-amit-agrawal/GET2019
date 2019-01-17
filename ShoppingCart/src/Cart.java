884700847ff9fa80fee3e16057ed46741c45de96
meta-amit-agrawal <amit.agrawal@metacube.com>
2019-01-17 14:04:07 +0530

    assignment 1

--- ShoppingCart/src/Cart.java
+++ ShoppingCart/src/Cart.java
@@ -1,6 +1,126 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
-public class Cart {
+public class Cart  {
+	
 	public static ArrayList<Item> cart=new ArrayList<Item>();
+	Scanner sc=new Scanner(System.in);
+	int cartChoice;
+	public void addItem(Item i)
+	{
+		Item i1;
+		int j,flag=0;
+		for(j=0;j<cart.size();j++)
+		{
+			i1=cart.get(j);
+			if(i.getProductId()==i1.getProductId())
+			{
+				i1.setProductQty(i1.getProductQty()+i.getProductQty());
+				flag=1;
+			}
+		}
+		if(flag!=1)
+		{
+			cart.add(i);
+		}
+		
+	}
+	
+	public void removeItem(int id)
+	{
+		int i,remove_qty;
+		Item i1;
+		
+			for(i=0;i<cart.size();i++)
+			{
+			i1=cart.get(i);
+			if(i1.getProductId()==id)
+			{
+				System.out.println("Enter the quantity to be removed...");
+				while((remove_qty=sc.nextInt())>i1.getProductQty())
+				{
+					System.out.println("Quantity present in cat is less than the entered quantity!!!");
+					System.out.println("\nPlease Re-enter the quantity...");
+				}
+				if((i1.getProductQty()-remove_qty)==0)
+				{
+					cart.remove(i);
+				}
+				else
+				{
+				i1.setProductQty(i1.getProductQty()-remove_qty);
+				}
+				System.out.println("Item updated Successfully");
+			}
+			else
+			{
+				System.out.println("Item Not Found...");
+			}
+			
+		}
+	}
+	
+	public void finalBill()
+	{
+		if(isCartEmpty())
+		{
+			System.out.println("Cart is Empty!!!");
+		}
+		else
+		{
+			int i;
+			Item  i1;
+			float finalbillamount=0.0f;
+			System.out.println("\n\nYour Final Cart is as follows");
+			showCart();
+			for(i=0; i<cart.size(); i++)
+			{
+				i1=cart.get(i);
+				finalbillamount= finalbillamount+(i1.getProductPrice()*i1.getProductQty());
+				
+			}
+		System.out.println("\nYour Final amount is......"+finalbillamount);
+		cart.clear();;
+		}
+	}
+	
+	public void showCart()
+	{
+		Item i1;
+		int i;
+		if(isCartEmpty())
+		{
+			System.out.println("\nCart is empty!!!");
+		}
+		else
+		{
+		System.out.println("\nID\tName\tPrice\tQuantity");
+		for(i=0; i<cart.size(); i++)
+		{
+			
+			i1=cart.get(i);
+			System.out.println("\n"+i1.getProductId()+"\t"+i1.getProductName()+"\t"+i1.getProductPrice()+"\t"+i1.getProductQty());
+	//		showCartMenu();
+		}
+		}
+		
+	}
+	public void showCartMenu()
+	{
+		System.out.println("Choose the option for cart");
+		System.out.println("2. Remove an Item");
+		System.out.println("3. Modify an Item");
+		System.out.println("4. Generate final bill");
+		cartChoice=sc.nextInt();
+	}
+	public boolean isCartEmpty()
+	{
+		if(cart.size()==0)
+		{
+			return true;
+		}
+		return false;
+	}
+	
 }
--- ShoppingCart/src/Menu.java
+++ ShoppingCart/src/Menu.java
@@ -10,10 +10,13 @@ public class Menu extends Cart {
 		System.out.println("Choose the option");
 		System.out.println("1. Add item in the list");
 		System.out.println("2. Add item in cart");
-		System.out.println("3. Go to cart");
-		System.out.println("4. Exit");
+		System.out.println("3. Remove item from cart");
+		System.out.println("4. Generate final bill");
+		System.out.println("5. Show Cart Items");
+		System.out.println("6. Show item in store");
+		System.out.println("7. Exit");
 	}
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
@@ -23,14 +26,36 @@ public class Menu extends Cart {
 		int choice,i,temp_qty;
 		float temp;
 		choice=sc.nextInt();
-		while(choice!=4)
+		while(choice!=7)
 		{
 			 i1=new Item();
 		switch(choice)
 		{
 		case 1:
+			Item i6;
+			int temp_id = 0;
 			System.out.println("Enter the ID of the Item");
-			i1.setProductId(sc.nextInt());
+			if(store.isEmpty())
+			{
+				temp_id=sc.nextInt();
+				i1.setProductId(temp_id);
+			}
+			else
+			{
+				for(int k=0; k<store.size(); k++)
+				{
+						temp_id=sc.nextInt();
+						i6=store.get(k);
+						if(temp_id==i6.getProductId())
+						{
+							System.out.println("ID already Exists...");
+							System.out.println("Please Re-enter the ID");
+							temp_id=sc.nextInt();
+						}
+						i1.setProductId(temp_id);
+						break;
+				}
+			}
 			System.out.println("Enter the name of the Item");
 			i1.setProductName(sc.next());
 			System.out.println("Enter the price of the Item");
@@ -58,41 +83,121 @@ public class Menu extends Cart {
 			
 			break;
 		case 2:
-			Item i2;
-			for(i=0;i<store.size();i++)
-			 {
-				 i2=store.get(i);
-				 System.out.println(i2.getProductId()+"  "+i2.getProductName()+"  "+i2.getProductPrice()+"  "+i2.getProductQty());
-			 }
-			 System.out.println("Enter the item ID to add in cart..");
-			 int id,flag=0;
-			 id=sc.nextInt();
-			 for(i=0;i<store.size();i++)
-			 {
-				 i2=store.get(i);
-				 if(id==i2.getProductId())
+			Item i2 = null;
+			Item i3 = null;
+			Cart c1 = null;
+			if(store.isEmpty())
+			{
+				System.out.println("Item List is Empty!!");
+			}
+			else
+			{
+				System.out.println("ID\tName\tPrice\tQuantity");
+				for(i=0; i<store.size(); i++)
 				 {
-					if(Cart.cart.add(i2))
-					{
-						flag=1;
-					}
-					break;
+					 i2 = store.get(i);
+					 
+					 System.out.println(i2.getProductId()+"\t"+i2.getProductName()+"\t"+i2.getProductPrice()+"\t"+i2.getProductQty());
 				 }
-			 }
-			 if(flag==1)
-			 {
-				 System.out.println("Item Added to cart successfully");
-			 }
-			 else
-			 {
-				 System.out.println("Item not found!!!");
-			 }
+				
+				 System.out.println("Enter the item ID to add in cart");
+				 int id,flag=0,flag_qty=0,qty;
+				 id=sc.nextInt();
+				 System.out.println("Enter the quantity to add in cart");
+				 qty=sc.nextInt();
+				 
+				 for(i=0;i<store.size();i++)
+				 {
+					 i2=store.get(i);
+					 if(id==i2.getProductId())
+					 {
+						 flag=1;
+						 if(qty<=i2.getProductQty())
+						 {
+							 i2.setProductQty(i2.getProductQty()-qty);
+							 flag_qty=1;
+							 break;
+						 }
+					 }
+				 }
+				 
+				 if(flag==1)
+				 {
+					 if(flag_qty==1)
+					 {
+						 i3=new Item();
+						 i3.setProductId(i2.getProductId());
+						 i3.setProductName(i2.getProductName());
+						 i3.setProductQty(qty);
+						 i3.setProductPrice(i2.getProductPrice());
+						 c1=new Cart();
+						 c1.addItem(i3);
+						 System.out.println("Item added in cart successfully");
+					 }
+					 else
+					 {
+						 System.out.println("Required Quantity is not available in Stock");
+					 }
+				 }
+				 else
+				 {
+					 System.out.println("Item not found!!!");
+				 }
+			}
+			
 			break;
+			
 		case 3:
+			
+			Cart c2=new Cart();
+			
+			if(c2.isCartEmpty())
+			{
+				System.out.println("Cart is empty");
+			}
+			else
+			{
+				c2.showCart();
+				System.out.println("Enter the id of the item to be removed...");
+				c2.removeItem(sc.nextInt());
+			}
+			
 			break;
-		case 4: 
+		
+		case 4:
+			Cart  c3=new Cart();
+			c3.finalBill();
+			break;
+			
+		case 5:
+			Cart c4=new Cart();
+			c4.showCart();
+			break;
+			
+		case 6:
+			Item i5;
+			if(store.isEmpty())
+			{
+				System.out.println("Item List is Empty!!");
+			}
+			else
+			{
+				System.out.println("ID\tName\tPrice\tQuantity");
+				for(i=0; i<store.size(); i++)
+				 {
+					 i5 = store.get(i);
+					 
+					 System.out.println(i5.getProductId()+"\t"+i5.getProductName()+"\t"+i5.getProductPrice()+"\t"+i5.getProductQty());
+				 }
+			}
+			break;
+			
+		case 7:
+			
 			System.exit(0);
+			
 		default:
+			
 			System.out.println("Invalid choice!!");
 		}
 		m.showMenu();
